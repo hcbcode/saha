244c5c631d612c8172293152de66545a02c26d4c
Steven Hadley <steven.hadley@neonstingray.com>
2013-07-21 13:02:19 +1000

    Refactored home screen to decouple Face Recog

--- SAHA/src/main/java/com/hcb/saha/activity/MainActivity.java
+++ SAHA/src/main/java/com/hcb/saha/activity/MainActivity.java
@@ -3,14 +3,16 @@ package com.hcb.saha.activity;
 import java.util.List;
 
 import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.WindowManager;
 
 import com.bugsense.trace.BugSenseHandler;
 import com.google.inject.Inject;
@@ -35,102 +37,24 @@ public class MainActivity extends RoboActivity {
 
 	@Inject
 	private Bus eventBus;
-	@InjectView(R.id.register)
-	private Button register;
-	@InjectView(R.id.recognize)
-	private Button recognize;
-	@InjectView(R.id.train)
-	private Button train;
-	@InjectView(R.id.listUsers)
-	private Button listUsers;
-	@InjectView(R.id.deleteUsers)
-	private Button deleteUsers;
 	@Inject
 	NativeFaceRecognizer faceRecognizer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		goFullScreen();
+		customiseActionBar();
 		setContentView(R.layout.activity_main);
-
-		register.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this,
-						RegisterActivity.class));
-			}
-		});
-
-		recognize.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// SahaFileManager.createFaceRecModelFile();
-				
-				startActivity(new Intent(MainActivity.this, IdentificationActivity.class));
-				
-				// FIXME Temp test data until the cropping works
-
-				//faceRecognizer.initRecognizer();
-//				String[][] idImageArray = new String[2][];
-//				idImageArray[0] = new String[] {
-//						"/sdcard/face/andreas1.jpg", "/sdcard/face/andreas2.jpg", "/sdcard/face/andreas3.jpg" };
-//				idImageArray[1] = new String[] {
-//						"/sdcard/face/kate1.jpg", "/sdcard/face/kate2.jpg", "/sdcard/face/kate3.jpg" };
-				
-				//faceRecognizer.trainRecognizer(idImageArray);
-				//eventBus.post(new FaceRecognitionEvents.TrainRecognizerRequest(idImageArray));
-				//eventBus.post(new FaceRecognitionEvents.PredictUserRequest("/sdcard/face/predict2.jpg"));
-				
-				//faceRecognizer.closeRecognizer();
-				
-			}
-		});
-		
-		train.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				List<User> users = SahaUserDatabase
-						.getAllUsers(MainActivity.this);
-				UsersFaces uf = SahaFileManager.getAllUsersFaceImages(users);
-				eventBus.post(new FaceRecognitionEvents.TrainRecognizerRequest(uf));
-			}
-		});
-
-		listUsers.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				List<User> users = SahaUserDatabase
-						.getAllUsers(MainActivity.this);
-				Log.d("SAHA", "users: " + users.size());
-				for (User user : users) {
-					Log.e("SAHA",
-							"User #" + user.getId() + ", name: "
-									+ user.getName() + ", dir: "
-									+ user.getDirectory());
-				}
-			}
-		});
-		
-		deleteUsers.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				SahaUserDatabase.deleteAllUsers(MainActivity.this);
-			}
-		});
 
 		eventBus.register(this);
 		eventBus.post(new LifecycleEvents.MainActivityCreated());
 	}
-	
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-
 		if (EnvConfig.USE_REPORTING) {
 			BugSenseHandler.initAndStartSession(this, SahaConfig.BUGSENSE_KEY);
 		}
@@ -154,9 +78,95 @@ public class MainActivity extends RoboActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_list_users:
+			List<User> users = SahaUserDatabase.getAllUsers(MainActivity.this);
+			Log.d("SAHA", "users: " + users.size());
+			for (User user : users) {
+				Log.e("SAHA",
+						"User #" + user.getId() + ", name: " + user.getName()
+								+ ", dir: " + user.getDirectory());
+			}
+			return true;
+		case R.id.action_delete_users:
+			SahaUserDatabase.deleteAllUsers(MainActivity.this);
+			return true;
+		case R.id.action_add_user:
+			startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+			return true;
+		case R.id.action_train:
+			List<User> trainUsers = SahaUserDatabase
+					.getAllUsers(MainActivity.this);
+			UsersFaces uf = SahaFileManager.getAllUsersFaceImages(trainUsers);
+			eventBus.post(new FaceRecognitionEvents.TrainRecognizerRequest(uf));
+			return true;
+		case R.id.action_recognize:
+			// SahaFileManager.createFaceRecModelFile();
+
+			startActivity(new Intent(MainActivity.this,
+					IdentificationActivity.class));
+
+			// FIXME Temp test data until the cropping works
+
+			// faceRecognizer.initRecognizer();
+			// String[][] idImageArray = new String[2][];
+			// idImageArray[0] = new String[] {
+			// "/sdcard/face/andreas1.jpg", "/sdcard/face/andreas2.jpg",
+			// "/sdcard/face/andreas3.jpg" };
+			// idImageArray[1] = new String[] {
+			// "/sdcard/face/kate1.jpg", "/sdcard/face/kate2.jpg",
+			// "/sdcard/face/kate3.jpg" };
+
+			// faceRecognizer.trainRecognizer(idImageArray);
+			// eventBus.post(new
+			// FaceRecognitionEvents.TrainRecognizerRequest(idImageArray));
+			// eventBus.post(new
+			// FaceRecognitionEvents.PredictUserRequest("/sdcard/face/predict2.jpg"));
+
+			// faceRecognizer.closeRecognizer();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Sample action bar we could use. Discuss.
+	 */
+	private void customiseActionBar() {
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+			ActionBar actionBar = getActionBar();
+			actionBar.setDisplayShowTitleEnabled(false);
+			actionBar.setDisplayShowHomeEnabled(false);
+			actionBar.setDisplayShowCustomEnabled(true);
+			LayoutInflater inflator = (LayoutInflater) this
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = inflator.inflate(R.layout.action_bar, null);
+			actionBar.setCustomView(v);
+		}
+	}
+
+	/**
+	 * The 'main' server application needs to be full screen. It doesn't need to
+	 * follow Android convention.
+	 * 
+	 * Discuss.
+	 */
+	private void goFullScreen() {
+		getWindow().addFlags(
+				WindowManager.LayoutParams.FLAG_FULLSCREEN
+						| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LOW_PROFILE);
+
+		}
+
 	}
 
 }
